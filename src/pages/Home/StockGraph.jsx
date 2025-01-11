5dc5f717d6b02e1010180a8608ded2553e6d6835
Anuprita <anupritamhapankar22@gmail.com>
2025-01-11 12:18:59 +0530

    chart added

--- src/pages/Home/StockGraph.jsx
+++ src/pages/Home/StockGraph.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../../assets/bitcoinLogo.png";
 import ButtonComponent from "../../commonComponents/ButtonComponent";
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import styles from "./styles.module.scss";
 import axios from "axios";
+import ChartComponent from "../../commonComponents/ChartComponent";
 const stocks = {
   Bitcoin: {
     name: "Bitcoin",
@@ -16,22 +18,24 @@ const stocks = {
   },
 };
 
-const fetchGraphData = async () => {
-  //   const response = await axios.get(
-  //     "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr%2Cusd&include_24hr_change=true%27?x_cg_demo_api_key=CG-JLUiV4oYV3siBiSKEV4HEZ9h"
-  //   );
-  //   if (response.data) {
-  // console.log({ respdata: response.data });
-  const responseObj = {
-    inr: 8057078,
-    usd: 93528,
-  };
-  // setGraph(responseObj);
-  // setGraph(response.data);
-  //   }
-};
 
 const StockGraph = () => {
+  const [graph, setGraph] = useState(null);
+  const fetchGraphData = async () => {
+    //   const response = await axios.get(
+    //     "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr%2Cusd&include_24hr_change=true%27?x_cg_demo_api_key=CG-JLUiV4oYV3siBiSKEV4HEZ9h"
+    //   );
+    //   if (response.data) {
+    // console.log({ respdata: response.data });
+    const responseObj = {
+      inr: 8057078,
+      usd: 93528,
+    };
+    setGraph(responseObj);
+    // setGraph(response.data);
+    //   }
+  };
+
   useEffect(() => {
     fetchGraphData();
   }, []);
@@ -45,7 +49,37 @@ const StockGraph = () => {
         />
         <p className={styles.stockName}>{stocks.Bitcoin.name}</p>
         <p className={styles.stockShortName}>{stocks.Bitcoin.shortName}</p>
-        <ButtonComponent children={"Rank#" + stocks.Bitcoin.rank} />
+        <ButtonComponent children={"Rank#" + stocks.Bitcoin.rank} className={styles.stockRank}/>
+      </div>
+
+      <div className={styles.priceDetails}>
+        <div className={styles.usdPriceContainer}>
+          <p className={styles.usdPrice}>$ {graph && graph?.usd.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
+          <p className={styles.percentangeChange}><ArrowDropUpIcon /> 2.51%</p>
+          <p className={styles.timeFormat}> (24H) </p>
+        </div>
+        
+        <p className={styles.inrPrice}>&#8377; {graph && graph?.inr.toLocaleString("en-US", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
+      </div>
+
+      <div className={styles.chartLegendContainer}>
+        <div className={styles.chartLegend}>
+          Bitcoin Price Chart (USD)
+        </div>
+        <div className={styles.chartDayOptions}>
+          <div className={styles.chartDayOptionsButton}>1H</div>
+          <div className={styles.chartDayOptionsButton}>24H</div>
+          <div className={styles.chartDayOptionsButtonActive}>7D</div>
+          <div className={styles.chartDayOptionsButton}>1M</div>
+          <div className={styles.chartDayOptionsButton}>3M</div>
+          <div className={styles.chartDayOptionsButton}>6M</div>
+          <div className={styles.chartDayOptionsButton}>1Y</div>
+          <div className={styles.chartDayOptionsButton}>All</div>
+        </div>
+      </div>
+
+      <div className={styles.chart}>
+        <ChartComponent />
       </div>
     </div>
   );
