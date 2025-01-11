e606d9e72decc6947d6d98b5480bfdf885c85c19
Anuprita <anupritamhapankar22@gmail.com>
2025-01-11 17:29:00 +0530

    analyst estimate

---
+++ src/components/Sentiment/AnalystEstimate.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ToolTipComponent from "../../commonComponents/ToolTipComponent";
+import InfoIcon from "@mui/icons-material/Info";
+import styles from "./styles.module.scss";
+
+const AnalystEstimate = () => {
+  return (
+    <div className={styles.analystEstimate}>
+      <p className={styles.subheading}>
+        Analyst Estimates
+        <ToolTipComponent
+          title="Analyst Estimates"
+          children={<InfoIcon className={styles.infoicon} />}
+          className={styles.tooltipStyle}
+        />
+      </p>
+      <div className={styles.analystEstimateContainer}>
+        {/* Left Section */}
+        <div className={styles.analysisPercentage}>
+          76<span className={styles.percentIcon}>%</span>
+        </div>
+        {/* Right section */}
+        <div className={styles.progressSection}>
+          <p className={styles.buyStatus}>Buy</p>
+          <div className={styles.buyStatusBox}> <div className={styles.buyPercent}></div>76%</div>
+          <p className={styles.buyStatus}>Hold</p>
+          <div className={styles.buyStatusBox}> <div className={styles.holdPercent}></div>8%</div>
+          <p className={styles.buyStatus}>Sell</p>
+          <div className={styles.buyStatusBox}> <div className={styles.sellPercent}></div>16%</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnalystEstimate;
--- src/components/Sentiment/Sentiment.jsx
+++ src/components/Sentiment/Sentiment.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import KeyEvents from './KeyEvents'
 import styles from "./styles.module.scss";
+import AnalystEstimate from './AnalystEstimate';
 
 const Sentiment = () => {
   return (
     <div className={styles.sentiment}>
       <p className={styles.heading}>Sentiment</p>
       <KeyEvents />
+      <AnalystEstimate />
     </div>
   )
 }
